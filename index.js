ba9f3639d04551d7dad381c52ceb5fc30b6107be
Sumit Pokhriyal <sumitstan1998@gmail.com>
2023-06-04 10:27:07 +0530

    middleware for cors

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(cors());
 app.use(express.json());
 
